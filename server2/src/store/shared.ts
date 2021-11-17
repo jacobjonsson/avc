c376677d3ec37c0c9110aae8308a6c2728a1f6d9
Jacob Jonsson <jacobjjonsson@gmail.com>
2021-11-17 16:23:18 +0100

    remove redundant files

--- server2/src/store/shared.ts
+++
@@ -1,8 +0,0 @@
-import {QueryResult} from "pg";
-
-// An abstraction over the postgres client to make it easy
-// to pass around the database connection and query functions
-export interface PGClient {
-  query<T>(sql: string): Promise<QueryResult<T>>;
-  query<T>(sql: string, args: any[]): Promise<QueryResult<T>>;
-}
